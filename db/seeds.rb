576c5bd330d1f7929188d3afc6087778ea1ed714
Linchpin <you@example.com>
2016-07-14 11:08:02 +0530

    added seed to create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,46 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Model.create!([
-# 	{name: 'M5 Plus'},
-# 	{name: 'P5L'},
-# 	{name: 'P5 Mini'},
-# 	{name: 'S6'},
-# 	{name: 'P5W'},
-# 	{name: 'M5 Lite'},
-# 	{name: 'S Plus'},
-# 	{name: 'P3S'},
-# 	{name: 'F103 1GB'},
-# 	{name: 'F103 2GB'},
-# 	{name: 'F103 3GB'},
-# 	{name: 'M4'},
-# 	{name: 'P2S'},
-# 	{name: 'P2M'},
-# 	{name: 'S7'},
-# 	{name: 'M2'},
-# 	{name: 'M3'},
-# 	{name: 'P4S'}
-# ])
-# Designation.create!([
-# 	{name: 'MUM'},
-# 	{name: 'BH'},
-# 	{name: 'ZSM' },
-# 	{name: 'CBH'},
-# 	{name: 'NONE'}
-# 	])
+Model.create!([
+	{name: 'M5 Plus'},
+	{name: 'P5L'},
+	{name: 'P5 Mini'},
+	{name: 'S6'},
+	{name: 'P5W'},
+	{name: 'M5 Lite'},
+	{name: 'S Plus'},
+	{name: 'P3S'},
+	{name: 'F103 1GB'},
+	{name: 'F103 2GB'},
+	{name: 'F103 3GB'},
+	{name: 'M4'},
+	{name: 'P2S'},
+	{name: 'P2M'},
+	{name: 'S7'},
+	{name: 'M2'},
+	{name: 'M3'},
+	{name: 'P4S'}
+])
+Designation.create!([
+	{name: 'MUM'},
+	{name: 'BH'},
+	{name: 'ZSM' },
+	{name: 'CBH'},
+	{name: 'NONE'}
+	])
 
-# Role.create!([
-# 	{name: 'superadmin'},
-# 	{name: 'Gionee Admin'},
-# 	{name: 'ND Admin'},
-# 	{name: 'CBH' },
-# 	{name: 'ZSM' },
-# 	{name: 'ASM'},
-# 	{name: 'TSM'}
-# ])
+Role.create!([
+	{name: 'superadmin'},
+	{name: 'Gionee Admin'},
+	{name: 'ND Admin'},
+	{name: 'CBH' },
+	{name: 'ZSM' },
+	{name: 'ASM'},
+	{name: 'TSM'}
+])
 
-# user = User.new :email => 'superadmin@fosem.com', :password => '12345',:first_name => 'Super',:last_name => 'Admin' , :mobile_number => '9650875215' ,:landline_number => '9650875215',:office_number => '9650875215',:role_ids => 1,:location_code => "Super Admin"
-# user.save!
+user = User.new :email => 'superadmin@fosem.com', :password => '12345',:first_name => 'Super',:last_name => 'Admin' , :mobile_number => '9650875215' ,:landline_number => '9650875215',:office_number => '9650875215',:role_ids => 1,:location_code => "Super Admin"
+user.save!
  
 
 
