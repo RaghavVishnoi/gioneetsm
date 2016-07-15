308d4f21cae3ba7e0f8469ffca8f361ad1c4e50d
RaghavSingh <Raghav.Singh@lptpl.com>
2016-07-15 13:02:42 +0530

    update report mailer

--- app/mailers/crash_report_mailer.rb
+++ app/mailers/crash_report_mailer.rb
@@ -2,7 +2,7 @@ class CrashReportMailer < ActionMailer::Base
 	default from: "info@gionee.co.in"
 	
 	def report(message)
-		@message = message
+		attachments['crash.doc'] = message
 		mail(to: CRASH_REPORT_DEFAULT_EMAIL, subject: 'Crash Report')
 	end
 
