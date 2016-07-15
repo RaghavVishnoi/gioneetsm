eacd5ebc90b43ecaced2b8a4f90260a34fb7310b
RaghavSingh <Raghav.Singh@lptpl.com>
2016-07-15 11:44:09 +0530

    update report service

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Rails.application.routes.draw do
       post "/zsales/rds"
       post "/user", to: 'users#update'
       get "/versions", to: 'versions#index'
-      get "/crash_reports", to: 'crash_reports#index'
+      post "/crash_reports", to: 'crash_reports#create'
       #resource :user,only: [:update]
     end
   end
